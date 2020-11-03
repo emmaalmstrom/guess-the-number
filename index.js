3d2cf3f4ccd5cb671090caddc48befd2de8894ad
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-11-03 10:17:30 +0100

    first commi

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Read instructions. And remove this line.
+
+function randomInt() {
+    return Math.floor(Math.random() * 101)
+}
+
+
+const secretNumber = randomInt()
+
+
+function getGuess() {
+    const stringValue = document.getElementById('user-input').value
+    return parseInt(stringValue, 10)
+}
+
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        const guess = getGuess()
+        console.log(guess)
+    }
+})
+
+function setMessage(msg) {
+    document.getElementById('message').innerText = msg
+}
+
+setMessage("bananpannkaka")
+const x = setMessage("banan")
+
+console.log(x)
