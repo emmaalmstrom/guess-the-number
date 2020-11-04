ccdc4b5308d25e2de4d79a76270b3a12e499f607
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-11-04 13:59:46 +0100

    add feedback to user

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 
+const secretNumber = randomInt()
+
 function randomInt() {
     return Math.floor(Math.random() * 101)
 }
-
-
-const secretNumber = randomInt()
-
 
 function getGuess() {
     const stringValue = document.getElementById('user-input').value
@@ -16,14 +14,19 @@ document.addEventListener('keyup', function (event) {
     if (event.key === 'Enter') {
         const guess = getGuess()
         console.log(guess)
+        
+        if (guess < secretNumber) {
+            setMessage('Too small!')
+        }
+        if (guess > secretNumber) {
+            setMessage('Too big!')
+        }
+        if (guess === secretNumber) {
+            setMessage('You are right!')
+        }
     }
 })
 
 function setMessage(msg) {
     document.getElementById('message').innerText = msg
 }
-
-setMessage("bananpannkaka")
-const x = setMessage("alman")
-
-console.log(x)
